5c2d5f60ca7e604d0f04fd58f6fec90fb7e0d2f4
PavelGozhii <pavelgozhii@gmail.com>
2021-06-02 09:31:52 +0300

    Change host

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
     port: 9000,
-    host: 'http://ec2-18-185-249-59.eu-central-1.compute.amazonaws.com'
+    host: 'ec2-18-185-249-59.eu-central-1.compute.amazonaws.com'
   }
 }
